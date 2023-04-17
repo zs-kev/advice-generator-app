5bf93bb4a124aab686f51ced1cfcdd1d6ff4f786
Kevin Simon <keviniansimon@gmail.com>
2023-04-17 17:04:50 +0200

    Finalised mobile styling

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,18 +2,25 @@ import React from 'react';
 
 import { FetchAdvice } from '../../api/fetch-advice';
 
+import './app.css';
+
 import Advice from '../Advice';
 import AdviceGeneratorButton from '../AdviceGeneratorButton';
 
 function App() {
   const getAdvice = FetchAdvice();
-  console.log(getAdvice);
 
   return (
-    <div>
+    <div className="advice-container">
       {getAdvice.status === 'loading' && <p>Loading...</p>}
       {getAdvice.status === 'success' && (
         <Advice advice={getAdvice.advice.slip} />
+      )}
+      {getAdvice.status === 'error' && (
+        <p className="error-state">
+          Looks like something went wrong. Try again by pressing the
+          button below
+        </p>
       )}
       <AdviceGeneratorButton newAdvice={getAdvice.handleClick} />
     </div>

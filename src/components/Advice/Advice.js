f93244206ef8452ef86bf21baa0e5ab4041a5c2a
Kevin Simon <keviniansimon@gmail.com>
2023-04-19 19:56:50 +0200

    Started adding desktop styling

--- src/components/Advice/Advice.js
+++ src/components/Advice/Advice.js
@@ -1,8 +1,10 @@
 import React from 'react';
 
+import './advice.css';
+
 function Advice({ advice }) {
   return (
-    <div>
+    <div className="advice">
       <h1>Advice #{advice.id}</h1>
       <p>"{advice.advice}"</p>
     </div>
